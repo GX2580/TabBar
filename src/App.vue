24e680041787736f104b968c1d907e09828b8c9e
gexiao <xiaoge740@gmail.com>
2024-06-13 16:23:56 +0800

    add some homePage

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,14 @@
 <script setup>
-import { ref, provide } from 'vue'
+import { ref, provide,shallowRef } from 'vue'
 import TabBar from './components/TabBar.vue'
 import TabBarItem from './components/TabBarItem.vue'
 
+import HomeView  from './views/HomeView.vue';
+import CateView from './views/CateView.vue';
+import CartView  from './views/CartView.vue';
+import UserView from './views/UserView.vue';
+
+const views = shallowRef([HomeView,CateView,CartView,UserView])
 // 定义响应式变量 active 和 items
 const active = ref(0)
 const items = ref([
@@ -12,11 +18,13 @@ const items = ref([
   { text: '我的', icon: 'user', index: 3 }
 ])
 
+
 // 向子组件提供 active 变量
 provide('active', active)
 </script>
 
 <template>
+  <component :is="views[active]"></component>
   <TabBar v-model="active">
     <!-- 使用 v-for 循环生成 TabBarItem 组件 -->
     <TabBarItem v-for="item in items" :key="item.index" :index="item.index" :icon="item.icon">
---
+++ src/views/CateView.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+  分类
+</template>
+
+<style scoped>
+</style>
